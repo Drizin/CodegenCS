3c5cdd4d5bd6de1af6eedb29532aaf8eaaffe8ad
Rick Drizin <Drizin@users.noreply.github.com>
2020-07-19 21:54:20 -0400

    Sample IDbConnectionFactory (used by POCOs ActiveRecord methods Save/Insert/Update)

---
+++ src/CodegenCS.POCO/IDbConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CodegenCS.AdventureWorksPOCOSample
+{
+    public class IDbConnectionFactory
+    {
+        public static IDbConnection CreateConnection()
+        {
+            string connectionString = @"Data Source=MYWORKSTATION\\SQLEXPRESS;
+                            Initial Catalog=AdventureWorks;
+                            Integrated Security=True;";
+
+            return new SqlConnection(connectionString);
+        }
+    }
+}
+
