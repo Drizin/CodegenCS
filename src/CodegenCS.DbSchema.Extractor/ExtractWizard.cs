c52b1ed74f23e8430cf45c40faab5c39f14dbbca
Rick Drizin <Drizin@users.noreply.github.com>
2021-02-28 14:51:57 -0500

    Splitting schema extractors into new project CodegenCS.DbSchema.Extractor.csproj

---
+++ src/CodegenCS.DbSchema.Extractor/ExtractWizard.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace CodegenCS.DbSchema.Extractor
+{
+    public class ExtractWizard
+    {
+        public DbTypeEnum? DbType { get; set; } = null;
+        public string ConnectionString { get; set; } = null;
+        public string OutputJsonSchema { get; set; } = null;
+
+
+        public enum DbTypeEnum
+        {
+            MSSQL,
+            PostgreSQL
+        }
+        public ExtractWizard()
+        {
+        }
+
+        public void Run()
+        {
+            string[] dbTypes = Enum.GetNames(typeof(DbTypeEnum));
+            while (DbType == null)
+            {
+                Console.WriteLine($"[Choose a Database Type]");
+                for (int i = 0; i < dbTypes.Length; i++)
+                    Console.WriteLine($"{i + 1}. {dbTypes[i]}");
+                Console.Write($"Database Type: ");
+                string chosen = Console.ReadLine();
+                int chosenInt;
+                if (int.TryParse(chosen, out chosenInt) && chosenInt >= 1 && chosenInt <= dbTypes.Length)
+                    DbType = Enum.Parse<DbTypeEnum>(dbTypes[chosenInt - 1]);
+            }
+            Console.WriteLine($"Database Type is {DbType}");
+            Console.WriteLine($"");
+
+            while(string.IsNullOrEmpty(ConnectionString))
+            {
+
+                Console.WriteLine($"[Choose a Connection String]");
+                switch (DbType.Value)
+                {
+                    case DbTypeEnum.MSSQL:
+                        Console.Write($"Example: Server=MYSERVER; Database=AdventureWorks; Integrated Security=True;");
+                        Console.Write($"Example: Server=MYSERVER; Database=AdventureWorks; User Id=myUsername;Password=myPassword");
+                        Console.Write($"Example: Server=MYWORKSTATION\\SQLEXPRESS; Database=AdventureWorks; Integrated Security=True;");
+                        break;
+                    case DbTypeEnum.PostgreSQL:
+                        Console.Write($"Example: Host=localhost; Database=Adventureworks; Username=postgres; Password=myPassword");
+                        break;
+                }
+
+                Console.Write($"Connection String: ");
+                ConnectionString = Console.ReadLine();
+            }
+
+            while (string.IsNullOrEmpty(OutputJsonSchema))
+            {
+
+                Console.WriteLine($"[Choose an Output File]");
+                Console.Write($"Output file: ");
+                OutputJsonSchema = Console.ReadLine();
+            }
+
+            switch (DbType.Value)
+            {
+                case DbTypeEnum.MSSQL:
+                    {
+                        Func<IDbConnection> connectionFactory = () => new System.Data.SqlClient.SqlConnection(ConnectionString);
+                        var reader = new PostgreSQL.PgsqlSchemaReader(connectionFactory);
+                        reader.ExportSchemaToJSON(OutputJsonSchema);
+                    }
+                    break;
+                case DbTypeEnum.PostgreSQL:
+                    {
+                        Func<IDbConnection> connectionFactory = () => new Npgsql.NpgsqlConnection(ConnectionString);
+                        var reader = new PostgreSQL.PgsqlSchemaReader(connectionFactory);
+                        reader.ExportSchemaToJSON(OutputJsonSchema);
+                    }
+                    break;
+            }
+
+
+
+            Console.Write($"Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/CodegenCS.DbSchema.Extractor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace CodegenCS.DbSchema.Extractor
+{
+    class Program
+    {
+        // Helpers to get the location of the current CS file
+        public static string GetScriptPath([System.Runtime.CompilerServices.CallerFilePath] string path = null) => path;
+        public static string GetScriptFolder([System.Runtime.CompilerServices.CallerFilePath] string path = null) => Path.GetDirectoryName(path);
+
+        static void Main(string[] args)
+        {
+            #region Command-Line Arguments
+            var argsParser = new Helpers.CommandLineArgsParser(args);
+            if (argsParser["?"] != null || argsParser["help"] != null)
+            {
+                ShowUsage();
+                Environment.Exit(0);
+            }
+            #endregion
+
+            //string outputJsonSchema = Path.GetFullPath(Path.Combine(GetScriptFolder(), @".\AdventureWorksSchema.json"));
+            var wizard = new ExtractWizard();
+            if (argsParser["postgresql"] != null)
+                wizard.DbType = ExtractWizard.DbTypeEnum.PostgreSQL;
+            else if (argsParser["mssql"] != null)
+                wizard.DbType = ExtractWizard.DbTypeEnum.MSSQL;
+            wizard.OutputJsonSchema = argsParser["output"];
+            wizard.ConnectionString = argsParser["cn"];
+
+
+            wizard.Run();
+        }
+
+        static void ShowUsage()
+        {
+            Console.WriteLine(string.Format("Usage: {0} [/postgresql | /mssql] [/cn=connectionString] [/output=path]", System.AppDomain.CurrentDomain.FriendlyName));
+            Console.WriteLine(string.Format(""));
+            Console.WriteLine(string.Format("Examples: {0} /postgresql /cn=\"Host=localhost; Database=Adventureworks; Username=postgres; Password=myPassword\" /output=AdventureWorks.json", System.AppDomain.CurrentDomain.FriendlyName));
+            Console.WriteLine(string.Format("Examples: {0} /mssql /cn=\"Server=MYSERVER; Database=AdventureWorks; User Id=myUsername;Password=myPassword;\" /output=AdventureWorks.json", System.AppDomain.CurrentDomain.FriendlyName));
+        }
+
+    }
+}
