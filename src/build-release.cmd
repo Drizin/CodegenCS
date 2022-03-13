6daa1f7296a2e904078bf33a8a18dc11d7bf3d5a
Rick Drizin <Drizin@users.noreply.github.com>
2022-03-13 14:35:09 -0400

    DotnetTool dotnet-codegencs depends on DbSchema.Extractor and DbSchema.Templates which were invalid for Sourcelink and Deterministic Builds because: - Missing Microsoft.SourceLink.GitHub - Missing EmbedUntrackedSources - Missing ContinuousIntegrationBuild - CodegenCS.DbSchema.Extractor was EXE (instead of DLL, even though the entry point is now dotnet-codegencs) and therefore it was generating another ".deps.json" file, and NuGet Package Explorer was failing when reading two .deps.json files.

--- src/build-release.cmd
+++ src/build-release.cmd
@@ -6,19 +6,38 @@ if not exist packages-local mkdir packages-local
 
 dotnet clean
 
+# CodegenCS + nupkg/snupkg
 dotnet build -c release CodegenCS\CodegenCS.csproj
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" /t:Pack CodegenCS\ /p:targetFrameworks="netstandard2.0;net472" /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /p:PackageOutputPath=..\packages-local\ /verbosity:minimal /p:ContinuousIntegrationBuild=true
 
+# CodegenCS.DbSchema + nupkg/snupkg
 dotnet build -c release CodegenCS.DbSchema\CodegenCS.DbSchema.csproj
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" /t:Pack CodegenCS.DbSchema\ /p:targetFrameworks="netstandard2.0;net472" /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /p:PackageOutputPath=..\packages-local\ /verbosity:minimal /p:ContinuousIntegrationBuild=true
 
 
-dotnet build -c release CodegenCS.DbSchema.Extractor\CodegenCS.DbSchema.Extractor.csproj
-dotnet build -c release CodegenCS.DbSchema.Templates\CodegenCS.DbSchema.Templates.csproj
+# Can clean again since dotnet-codegencs will use Nuget references
+dotnet clean
+rmdir /s /q .\dotnet-codegencs\bin\
+rmdir /s /q .\dotnet-codegencs\obj\
+rmdir /s /q .\CodegenCS.DbSchema.Extractor\bin\
+rmdir /s /q .\CodegenCS.DbSchema.Extractor\obj\
 
+
+# This DLL is packed inside dotnet-codegencs tool
+dotnet build -c release CodegenCS.DbSchema.Extractor\CodegenCS.DbSchema.Extractor.csproj
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" /t:Build CodegenCS.DbSchema.Extractor\ /p:targetFrameworks="netstandard2.0;net472" /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /verbosity:minimal /p:ContinuousIntegrationBuild=true
+
+# This DLL is packed inside dotnet-codegencs tool
+dotnet build -c release CodegenCS.DbSchema.Templates\CodegenCS.DbSchema.Templates.csproj
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" /t:Build CodegenCS.DbSchema.Templates\ /p:targetFrameworks="netstandard2.0;net472" /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /verbosity:minimal /p:ContinuousIntegrationBuild=true
+
+
+# dotnet-codegencs (DotnetTool nupkg/snupkg)
 dotnet build -c release dotnet-codegencs\dotnet-codegencs.csproj
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe" /t:Pack dotnet-codegencs\ /p:targetFrameworks="net5.0" /p:Configuration=Release /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg /p:PackageOutputPath=..\packages-local\ /verbosity:minimal /p:ContinuousIntegrationBuild=true
 dotnet tool uninstall -g dotnet-codegencs & dotnet tool install --global --add-source .\packages-local dotnet-codegencs
 
+
+# Unit tests
 dotnet build -c release CodegenCS.Tests\CodegenCS.Tests.csproj
 dotnet test  CodegenCS.Tests\CodegenCS.Tests.csproj
