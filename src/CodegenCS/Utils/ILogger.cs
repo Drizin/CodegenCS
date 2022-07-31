b9a1db6ffe64ddcf54500d4d667cd5bbe51057a3
Rick Drizin <Drizin@users.noreply.github.com>
2022-07-31 15:40:48 -0400

    CodegenCS.Utils.ILogger - CLI tools and VS Extensions (both the tools and the templates itself) should write to output using this: - CLI (dotnet-codegencs tools and launched templates) will write to output using ColoredConsoleLogger: an adapter that writes to InterpolatedColorConsole.ColoredConsole. - VS Extensions (toold and launched templates) will write to output using VsOutputWindowPaneOutputLogger: an adapter that writes to custom IVsOutputWindowPane. This one should ignore embedded colors (ColoredConsoleLogger will use/honor those color codes)
    Removed legacy/unused CommandLineArgsParser.cs

---
+++ src/CodegenCS/Utils/ILogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CodegenCS.Utils
+{
+    /// <summary>
+    /// Reusable tools should write all output to this interface.
+    /// Templates can also use this to write to output.
+    /// For CLI (dotnet-codegencs) this is implemented by an adapter that writes to InterpolatedColorConsole.ColoredConsole.
+    /// For VS Extensions this is implemented by an adapter that writes to custom IVsOutputWindowPane.
+    /// The implementations should handle embedded (interpolated) colors (e.g. ColoredConsole supports colors) or discard embedded colors.
+    /// </summary>
+    public interface ILogger
+    {
+        Task WriteLineAsync();
+        Task WriteLineAsync(FormattableString value);
+        Task WriteLineAsync(RawString value);
+        Task WriteLineAsync(ConsoleColor foregroundColor, FormattableString value);
+        Task WriteLineAsync(ConsoleColor foregroundColor, ConsoleColor backgroundColor, FormattableString value);
+        Task WriteLineAsync(ConsoleColor foregroundColor, RawString value);
+        Task WriteLineAsync(ConsoleColor foregroundColor, ConsoleColor backgroundColor, RawString value);
+
+        Task WriteLineErrorAsync();
+        Task WriteLineErrorAsync(FormattableString value);
+        Task WriteLineErrorAsync(RawString value);
+        Task WriteLineErrorAsync(ConsoleColor foregroundColor, FormattableString value);
+        Task WriteLineErrorAsync(ConsoleColor foregroundColor, ConsoleColor backgroundColor, FormattableString value);
+        Task WriteLineErrorAsync(ConsoleColor foregroundColor, RawString value);
+        Task WriteLineErrorAsync(ConsoleColor foregroundColor, ConsoleColor backgroundColor, RawString value);
+    }
+}
---
+++ src/dotnet-codegencs/ColoredConsoleLogger.cs
@@ -0,0 +1,94 @@
+﻿using CodegenCS.Utils;
+using System;
+using System.Threading.Tasks;
+using Console = InterpolatedColorConsole.ColoredConsole;
+
+namespace CodegenCS.DotNetTool
+{
+    internal class ColoredConsoleLogger : ILogger
+    {
+        public Task WriteLineAsync()
+        {
+            Console.WriteLine();
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineAsync(FormattableString value)
+        {
+            Console.WriteLine(value);
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineAsync(RawString value)
+        {
+            Console.WriteLine($"{value}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineAsync(ConsoleColor foregroundColor, FormattableString value)
+        {
+            Console.WriteLine(foregroundColor, value);
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineAsync(ConsoleColor foregroundColor, ConsoleColor backgroundColor, FormattableString value)
+        {
+            Console.WriteLine(foregroundColor, backgroundColor, value);
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineAsync(ConsoleColor foregroundColor, RawString value)
+        {
+            Console.WriteLine(foregroundColor, $"{value}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineAsync(ConsoleColor foregroundColor, ConsoleColor backgroundColor, RawString value)
+        {
+            Console.WriteLine(foregroundColor, backgroundColor, $"{value}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineErrorAsync()
+        {
+            Console.WriteLineError("");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineErrorAsync(FormattableString value)
+        {
+            Console.WriteLineError(value);
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineErrorAsync(RawString value)
+        {
+            Console.WriteLineError($"{value}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineErrorAsync(ConsoleColor foregroundColor, FormattableString value)
+        {
+            Console.WriteLineError(foregroundColor, value);
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineErrorAsync(ConsoleColor foregroundColor, ConsoleColor backgroundColor, FormattableString value)
+        {
+            Console.WriteLineError(foregroundColor, backgroundColor, value);
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineErrorAsync(ConsoleColor foregroundColor, RawString value)
+        {
+            Console.WriteLineError(foregroundColor, $"{value}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteLineErrorAsync(ConsoleColor foregroundColor, ConsoleColor backgroundColor, RawString value)
+        {
+            Console.WriteLineError(foregroundColor, backgroundColor, $"{value}");
+            return Task.CompletedTask;
+        }
+    }
+}
